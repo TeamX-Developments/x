f79864b7940741187c1d2c462cf35380cabb0dee
Popout Rage <dawnshafer05@gmail.com>
2025-12-12 05:33:45 -0500

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,5 @@
 // supabase-client.js
-const SUPABASE_URL = "https://YOUR_PROJECT_REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://qopgwxlrdqjyamerllwz.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_HYgmuRFRreMN0le5UX26PA_Nkbul_ae";
+
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
